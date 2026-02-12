2d96760bdf90f1221f099f80c67c23048efc5074
Paolo Cozzi <bunop@libero.it>
2026-02-12 02:12:45 +0100

    :recycle: restructure templates and add cineca.sh for HPC configuration

---
+++ templates/cineca.sh
@@ -0,0 +1,3 @@
+#SBATCH --account=<account>             # account name
+#SBATCH --partition=g100_usr_prod       # partition name (see https://docs.hpc.cineca.it/hpc/galileo.html#job-managing-and-slurm-partitions)
+#SBATCH --qos=g100_qos_lprod            # quality of service (see https://docs.hpc.cineca.it/hpc/galileo.html#job-managing-and-slurm-partitions)
--- {{cookiecutter.project_slug}}/scripts/launch.sh
+++ {{cookiecutter.project_slug}}/scripts/launch.sh
@@ -5,13 +5,11 @@
 #SBATCH --time=<time limit>
 #SBATCH --mem=16G
 #SBATCH --output=%x-%j.out              # using job_name + job_id as log file
-#SBATCH --job-name=methylseq
-#SBATCH --account=<your account name>
-#SBATCH --partition=<partition name>
-#SBATCH --qos=<qos name>
+#SBATCH --job-name=<job name>
+{% include "cineca.sh" %}
 
 # set the path of institution-specific configuration files
 export CUSTOM_CONFIG_BASE=${WORK}/nf-configs
 {% if cookiecutter.project_type == "nf-core/methylseq" %}
-{% include "launch-methylseq.sh" %}
+{% include "methylseq.sh" %}
 {% endif %}
