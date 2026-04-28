f653d15daf6f284cfcebab6bf07cc7c5d6cda923
Paolo Cozzi <paoloalessandro.cozzi@gmail.com>
2026-04-28 16:25:16 +0200

    :wrench: update wf-basecalling workflow version
    Updates the template to use `v1.5.9-mc` of the `bunop/wf-basecalling` pipeline, incorporating recent changes and improvements.

--- templates/wf-basecalling.sh
+++ templates/wf-basecalling.sh
@@ -1,5 +1,5 @@
 # mind to the pipeline version (required)
-nextflow run bunop/wf-basecalling -r v1.5.8 \
+nextflow run bunop/wf-basecalling -r v1.5.9-mc \
     --custom_config_base ${CUSTOM_CONFIG_BASE} \
     -config ${CUSTOM_CONFIG_BASE}/nfcore_custom.config \
     -config ${CUSTOM_CONFIG_BASE}/pipeline/wf-basecalling.config \
